51f0560304ea3c8a206eae184842b02d9271fe46
Devon Govett <devongovett@gmail.com>
2016-07-31 20:44:44 -0700

    Fix extending error object
    Broken in Babel 6.x.

--- src/underflow.js
+++ src/underflow.js
@@ -1,7 +1,7 @@
 // define an error class to be thrown if an underflow occurs
-export default class UnderflowError extends Error {
+export default class UnderflowError {
   constructor() {
-    super();
+    Error.call(this);
     this.name = 'UnderflowError';
     
     if (typeof Error.captureStackTrace === 'function') {
@@ -11,3 +11,13 @@ export default class UnderflowError extends Error {
     }
   }
 }
+
+// Extending Error doesn't work in Babel 6.x so do it manually.
+// See https://phabricator.babeljs.io/T3083.
+UnderflowError.prototype = Object.create(Error.prototype, {
+  constructor: {
+    value: UnderflowError
+  }
+});
+
+Object.setPrototypeOf(UnderflowError, Error);
